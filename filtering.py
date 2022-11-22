d08b25507d776c287c866ff08078c0155486c947
halanaman <81603736+halanaman@users.noreply.github.com>
2022-11-22 19:35:44 +0800

    Create filtering.py

---
+++ filtering.py
@@ -0,0 +1,71 @@
+ls = []
+ls_key = []
+ls_value = []
+dict_in = {}
+dict_out = {}
+with open("readme.csv","r") as f:
+    ls = f.read().split("\n")
+    ls_key = ls[0].split(",")
+    for i in range(1,len(ls)):
+        ls_value = ls[i].split(",")
+        dict_in = dict(zip(ls_key,ls_value))
+        dict_out[i] = dict_in
+    f.close()
+#print(dict_out)
+
+userpax = input("Enter no. of people: ")
+userOccasion = input("Enter Occasion: ")
+userLocation = input("Enter Location: ")
+usertime = input("Enter time: ")
+userPrice = input("Enter Price: ")
+
+filterpax = userpax != ""
+filterOccasion = userOccasion != ""
+filterLocation = userLocation != ""
+filtertime = usertime != ""
+filterPrice = userPrice != ""
+    
+dict_show = {}    
+
+for i in range(1,len(dict_out)):
+    """
+    print(float(dict_out[i]['pax_min']))
+    print(float(dict_out[i]['pax_max']))
+    print(dict_out[i]['Occasion'])
+    print(dict_out[i]['Location'])
+    print(float(dict_out[i]['time_min']))
+    print(float(dict_out[i]['time_max']))
+    print(dict_out[i]['Price'])
+    """
+    appendd = True
+    if filterpax:
+        if float(dict_out[i]['pax_min']) <= float(userpax) <= float(dict_out[i]['pax_max']):
+            pass
+        else:
+            appendd = False
+    if filterOccasion:
+        if userOccasion in dict_out[i]['Occasion']:
+            pass
+        else:
+            appendd = False
+    if filterLocation:
+        if userLocation in dict_out[i]['Location']:
+            pass
+        else:
+            appendd = False
+    if filtertime:
+        if float(dict_out[i]['time_min']) <= float(usertime) <= float(dict_out[i]['time_max']):
+            pass
+        else:
+            appendd = False
+    if filterPrice:
+        if userPrice == dict_out[i]['Price']:
+            pass
+        else:
+            appendd = False
+    if appendd:
+        dict_show[i] = dict_out[i]
+
+for key in dict_show:
+    print("*****")
+    print(dict_show[key])
